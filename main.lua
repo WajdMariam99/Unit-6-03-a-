0463d87447cee1db2a5b358de5b3675ecfefd9f2
WajdMariam99 <47641469+WajdMariam99@users.noreply.github.com>
2019-04-12 13:26:00 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -4,44 +4,51 @@
 --
 -----------------------------------------------------------------------------------------
 
-local EnterButton = display.newImageRect ("assets/sprites/enterbutton.png", 125, 50)
-EnterButton.x = 160
-EnterButton.Y = 350
-EnterButton.id = "Enter Button"
+local calculateButton = display.newImageRect ("assets/sprites/calculateButton.png", 125, 50)
+calculateButton.x = 160	
+calculateButton.y = 350 
+calculateButton.id = "calculateButton"
 
 
-ResponseText = display.newText ( " What movie can you watch? " , display.contentCenterX - 3, 160, native.systemFont, 25 )
-ResponseText:setFillColor ( 0/255 , 0/255, 0/255 )
+responseText = display.newText ( " What movie can you watch? " , display.contentCenterX - 3, 160, native.systemFont, 25 )
+responseText:setFillColor ( 255/255 , 255/255, 255/255 )
 
 
-display.newText (" Enter your age: " , display.contentCenterX, 380, native.systemFont, 30 )
+display.newText (" Enter your age: " , display.contentCenterX, 200, native.systemFont, 30 )
 
 
-AgeField = native.newTextField ( display.contentCenterX, display.contentCenterY + 35, 225, 40 )
-AgeField.id = "Age textField"
+ageField = native.newTextField ( display.contentCenterX, display.contentCenterY + 35, 225, 40 )
+ageField.id = "Age textField"
 
 
-local function EnterButtonTouch( Event )
-	end
+local function calculateButtonTouch( Event )
+	
 
-local Age = tonumber (AgeField.text)
+local nn = tonumber(ageField.text)
 
-if Age >= 18 then
-	print ("")
-    print ("You can watch horror movie alone")
-	ResponseText.text = "You can watch horror movie alone"
+if nn >= 17 then
 
-elseif Age >= 13 then
-	print ("")
-	print (" You can watch horror movie with someone else ")
-	ResponseText.text = " You can watch horror movie with someone else "
+
+	
+	responseText.text = "You can watch horror movie alone"
+
+
+
+elseif nn >= 13 then
+
+
+	
+	responseText.text = " You can watch horror movie with someone else "
 
 else 
-	print ("")
-	print (" You can't watch horror movie, you are too young!")
-	ResponseText.text = " You can't watch horror movie, you are too young!"
+
+	
+	responseText.text = " You can't watch horror movie, you are too young!"
+
+end
 
 end
 
 
-EnterButton:addEventListener ("Touch" , EnterButtonTouch)
+
+calculateButton:addEventListener("Touch" , calculateButtonTouch)
